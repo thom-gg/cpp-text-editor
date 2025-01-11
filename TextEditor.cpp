3523e38bc8ec1d26748decce75051707303ebd3d
thom-gg <79939935+thom-gg@users.noreply.github.com>
2025-01-11 23:20:32 +0100

    Drawing line number and taking newline into consideration (+ code formatting)

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -16,35 +16,36 @@
 #include "SillyCat.hpp"
 #include "CatFactory.hpp"
 
-
-void TextEditor::fileHasBeenOpened(QString & content) {
-    if (this->textEdit == nullptr) {
+void TextEditor::fileHasBeenOpened(QString &content)
+{
+    if (this->textEdit == nullptr)
+    {
         setupQTextEdit();
         factory->startTimer();
     }
-    
 
     // Change the content of the text editor
     this->textEdit->setPlainText(content);
     // Set cursor initial position
-    QTextCursor cursor = textEdit->textCursor();        
+    QTextCursor cursor = textEdit->textCursor();
     cursor.movePosition(QTextCursor::End);
-    textEdit->setTextCursor(cursor);    
+    textEdit->setTextCursor(cursor);
 }
 
-void TextEditor::newEmptyFile() {
+void TextEditor::newEmptyFile()
+{
     // We emit a signal to be caught by FileManager to store the info that its not a real file we're working in currently
-    emit newEmptyFileRequested(); 
+    emit newEmptyFileRequested();
     // Open an editor with empty content
     QString emptyContent;
     this->fileHasBeenOpened(emptyContent);
-
-
 }
 
 // When the save button is pressed, or CTRL + S is pressed
-void TextEditor::saveFileTriggered() {
-    if (this->textEdit == nullptr) {
+void TextEditor::saveFileTriggered()
+{
+    if (this->textEdit == nullptr)
+    {
         return; // we're on welcome menu
     }
     // Emit signal to be caught by FileManager and actually saves the file. We pass the content (QString) as parameter
@@ -52,146 +53,162 @@ void TextEditor::saveFileTriggered() {
 
     QColor originalColor = textEdit->palette().color(QPalette::Base);
     // Little animation (light green blink), to visually notify that it has been saved
-     this->textEdit->setStyleSheet("QTextEdit { background-color:rgb(47, 54, 47) }");  
+    this->textEdit->setStyleSheet("QTextEdit { background-color:rgb(47, 54, 47) }");
 
-     // Create a single-shot timer to revert back to white after 200ms
-     QTimer::singleShot(100, this, [this, originalColor]() {
-        textEdit->setStyleSheet(QString("QTextEdit { background-color: %1 }")
-                                .arg(originalColor.name()));
-     });
-    
+    // Create a single-shot timer to revert back to white after 200ms
+    QTimer::singleShot(100, this, [this, originalColor]()
+                       { textEdit->setStyleSheet(QString("QTextEdit { background-color: %1 }")
+                                                     .arg(originalColor.name())); });
 }
 
+void TextEditor::setupWelcomeScreen(QWidget *textEditor)
+{
+    QVBoxLayout *mainLayout = new QVBoxLayout();
 
-void TextEditor::setupWelcomeScreen(QWidget * textEditor) {
-        QVBoxLayout * mainLayout = new QVBoxLayout();
-        
-        QPushButton* button1 = new QPushButton("New file", textEditor);
-        QPushButton* button2 = new QPushButton("Open file", textEditor);
-        QPushButton* button3 = new QPushButton("Open folder", textEditor);
+    QPushButton *button1 = new QPushButton("New file", textEditor);
+    QPushButton *button2 = new QPushButton("Open file", textEditor);
+    QPushButton *button3 = new QPushButton("Open folder", textEditor);
 
+    connect(button1, &QPushButton::clicked, this, &TextEditor::newEmptyFile);
+    connect(button2, &QPushButton::clicked, this, [this]()
+            { emit openFileRequested(); });
 
-        connect(button1, &QPushButton::clicked, this, &TextEditor::newEmptyFile);
-        connect(button2, &QPushButton::clicked, this, [this]() {emit openFileRequested();});
+    button1->setMinimumSize(100, 50); // Set minimum size for buttons
+    button2->setMinimumSize(100, 50);
+    button3->setMinimumSize(100, 50);
 
+    QHBoxLayout *row = new QHBoxLayout();
+    // Add buttons to the layouts
+    row->addSpacing(50);
+    row->addWidget(button1);
+    row->addSpacing(10);
+    row->addWidget(button2);
+    row->addSpacing(10);
+    row->addWidget(button3);
+    row->addStretch();
 
-        button1->setMinimumSize(100, 50); // Set minimum size for buttons
-        button2->setMinimumSize(100, 50);
-        button3->setMinimumSize(100, 50);
-       
-        QHBoxLayout * row = new QHBoxLayout();
-        // Add buttons to the layouts
-        row->addSpacing(50);
-        row->addWidget(button1);
-        row->addSpacing(10);
-        row->addWidget(button2);
-        row->addSpacing(10);
-        row->addWidget(button3);
-        row->addStretch();
+    mainLayout->addSpacing(50);
+    mainLayout->addLayout(row);
+    mainLayout->addStretch();
 
-        mainLayout->addSpacing(50);
-        mainLayout->addLayout(row);
-        mainLayout->addStretch();
-        
-
-        // Apply layout to the widget
-        textEditor->setLayout(mainLayout);
+    // Apply layout to the widget
+    textEditor->setLayout(mainLayout);
 }
 
-
-void TextEditor::updateZooming(int amount) {
+void TextEditor::updateZooming(int amount)
+{
     // will zoomOut if amount is negative
     textEdit->zoomIn(amount);
 }
 
 // Handle mouse wheel
-void TextEditor::wheelEvent(QWheelEvent *event)  {
+void TextEditor::wheelEvent(QWheelEvent *event)
+{
     const double degrees = event->angleDelta().y() / 8.0;
     updateZooming((degrees / 5));
-
 }
 
 // Called when we get out of welcome screen
-void TextEditor::setupQTextEdit() {
-        this->textEdit = new QTextEdit();
-        this->textEdit->zoomIn(5);
+void TextEditor::setupQTextEdit()
+{
+    this->textEdit = new QTextEdit();
+    this->textEdit->zoomIn(5);
 
-        QVBoxLayout * mainLayout = new QVBoxLayout(this);
-        mainLayout->setContentsMargins(0, 0, 0, 0);
-        mainLayout->setSpacing(0);
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+    mainLayout->setSpacing(0);
 
-        mainLayout->addWidget(this->textEdit);
-        if (this->layout()) {
-            delete this->layout(); // Clean up the old layout
-        }
-        this->setLayout(mainLayout);
+    mainLayout->addWidget(this->textEdit);
+    if (this->layout())
+    {
+        delete this->layout(); // Clean up the old layout
+    }
+    this->setLayout(mainLayout);
 
-        // Connect the "textChanged" signal from QTextEdit class to a custom one we're sending
-        // (why? because I plan on getting rid of QTextEdit class later and implement all of this myself)
-        connect(this->textEdit, &QTextEdit::textChanged, this, [this]() {emit signalFileHasBeenModified();});
+    // Connect the "textChanged" signal from QTextEdit class to a custom one we're sending
+    // (why? because I plan on getting rid of QTextEdit class later and implement all of this myself)
+    connect(this->textEdit, &QTextEdit::textChanged, this, [this]()
+            { emit signalFileHasBeenModified(); });
 }
 
+void TextEditor::printNewLine(int &xCoord, int &yCoord, int &charHeight, int &lineNumber, QPainter &painter)
+{
+    yCoord += charHeight;
+    lineNumber += 1;
+    painter.setPen(Qt::gray);
+    painter.drawText(0, yCoord, QString("%1:").arg(lineNumber));
+    painter.setPen(Qt::white);
 
-    void TextEditor::paintEvent(QPaintEvent *event)  {
-        std::cout << "paint event " << std::endl;
-        // Drawing text
-        QPainter painter(this);
-        painter.setPen(Qt::white);
-        // using a monospace font so we can count the width for each character and know where to break line
-        painter.setFont(QFont("Courier", 49));
+    xCoord = X_OFFSET;
+}
 
-        QRect bounds = painter.boundingRect(rect(), Qt::AlignLeft, "A");
-        int charWidth = bounds.width();
-        int charHeight = bounds.height();
+void TextEditor::paintEvent(QPaintEvent *event)
+{
+    std::cout << "paint event " << std::endl;
+    // Drawing text
+    QPainter painter(this);
+    painter.setPen(Qt::white);
+    // using a monospace font so we can count the width for each character and know where to break line
+    painter.setFont(QFont("Courier", 20));
 
-        int maxWidth = width();
+    QRect bounds = painter.boundingRect(rect(), Qt::AlignLeft, "A");
+    int charWidth = bounds.width();
+    int charHeight = bounds.height();
 
+    int maxWidth = width();
 
-        QString currWord("");
+    QString currWord("");
 
-        int xCoord = 0;
-        int yCoord = 10 + charHeight;
+    int lineNumber = 0;
+    int xCoord = X_OFFSET;
+    int yCoord = 10;
 
-        
-        for (int i = 0; i<textBuffer->length(); i++) {
-            char c = textBuffer->charAt(i);
-            QChar ch = c;
-            currWord += ch;
+    printNewLine(xCoord, yCoord, charHeight, lineNumber, painter);
 
-            if ( c == ' ') {
-                // print currWord if needed
-                int wordWidth = currWord.length() * charWidth;
-                if ( (xCoord + wordWidth) > maxWidth) {
-                    // break line
-                    xCoord = 0;
-                    yCoord += charHeight;
-                }
-                painter.drawText(xCoord,yCoord,currWord);
+    int length = textBuffer->length();
 
-                xCoord += wordWidth;
-                currWord.clear();
+    // We iterate on each char (and once after to print the last word)
+    for (int i = 0; i <= length; i++)
+    {
+        char c;
+        if (i < length)
+        {
+            c = textBuffer->charAt(i);
+
+            if (c != '\n')
+            { // we dont draw a newline char, we handle it ourselves using our printNewLine
+                QChar ch = c;
+                currWord += ch;
             }
-
-
         }
-        // draw last word if no spaces at the end
-        int wordWidth = currWord.length() * charWidth;
-        if ( (xCoord + wordWidth) > maxWidth) {
-            // break line
-            xCoord = 0;
-            yCoord += charHeight;
+
+        if (i == length || c == ' ' || c == '\n')
+        { // when we encounter a space or for last iteration
+            int wordWidth = currWord.length() * charWidth;
+            if ((xCoord + wordWidth) > maxWidth)
+            {
+                // break line if needed
+                xCoord = X_OFFSET;
+                yCoord += charHeight;
             }
-            painter.drawText(xCoord,yCoord,currWord);
+            // print the word we just completed
+            painter.drawText(xCoord, yCoord, currWord);
 
             xCoord += wordWidth;
             currWord.clear();
-         }
+        }
 
+        if (c == '\n')
+        {
+            printNewLine(xCoord, yCoord, charHeight, lineNumber, painter);
+        }
+    }
+}
 
-
-TextEditor::TextEditor() {
+TextEditor::TextEditor()
+{
     // Dummy data to try the paint function
-    textBuffer = new TextBuffer("Hello everyone this is a test text to try printing it out on the screen.", 72);
+    textBuffer = new TextBuffer("Hello !\nThis is a test text to try printing test on the screen by myself without using the QTextEdit component.", 111);
     //  setupWelcomeScreen(this);
 
     // // Bind CTRL+S to save slot
@@ -204,30 +221,29 @@ TextEditor::TextEditor() {
 
     // QShortcut * zoomOutShortcut = new QShortcut(QKeySequence::ZoomOut, this);
     // connect(zoomOutShortcut, &QShortcut::activated, this, [this]() {updateZooming(-10);});
-  
-    
-    // factory = new CatFactory(this);
 
-        
- 
+    // factory = new CatFactory(this);
 }
 
-TextEditor::~TextEditor() {
-    if (textBuffer != nullptr) {
+TextEditor::~TextEditor()
+{
+    if (textBuffer != nullptr)
+    {
         delete textBuffer;
     }
-    if (factory != nullptr) {
+    if (factory != nullptr)
+    {
         delete factory;
     }
-    if (textEdit != nullptr) {
+    if (textEdit != nullptr)
+    {
         delete textEdit;
     }
 }
 
-
-QPoint TextEditor::getCursorPosition() {
+QPoint TextEditor::getCursorPosition()
+{
     QPoint q = textEdit->mapToGlobal(textEdit->cursorRect().topLeft());
 
     return q;
-
 }
--- TextEditor.hpp
+++ TextEditor.hpp
@@ -9,31 +9,34 @@
 
 class CatFactory; // for circular dependencies
 
-class TextEditor: public QWidget {
+class TextEditor : public QWidget
+{
     Q_OBJECT
-    private:
-        QTextEdit * textEdit = nullptr;
-        CatFactory* factory = nullptr;
-        TextBuffer * textBuffer = nullptr;
-        void setupWelcomeScreen(QWidget *);
-        void updateZooming(int amount);
-        void setupQTextEdit();
+private:
+    int X_OFFSET = 50;
+    QTextEdit *textEdit = nullptr;
+    CatFactory *factory = nullptr;
+    TextBuffer *textBuffer = nullptr;
+    void setupWelcomeScreen(QWidget *);
+    void updateZooming(int amount);
+    void setupQTextEdit();
+    void printNewLine(int &xCoord, int &yCoord, int &charHeight, int &lineNumber, QPainter &painter);
 
-    public:
-        TextEditor();
-        ~TextEditor();
-        QPoint getCursorPosition();
-    signals:
-        void signalFileHasBeenModified(); // to be caught by MainWindow to update window title
-        void saveFileWithContent(QString);
-        void openFileRequested();
-        void newEmptyFileRequested(); // sending to FileManager that we are creating an empty file
-    public slots:
-        void fileHasBeenOpened(QString &);
-        void saveFileTriggered();
-        void newEmptyFile(); // receiving from CustomMenu that we need to create a new empty file
+public:
+    TextEditor();
+    ~TextEditor();
+    QPoint getCursorPosition();
+signals:
+    void signalFileHasBeenModified(); // to be caught by MainWindow to update window title
+    void saveFileWithContent(QString);
+    void openFileRequested();
+    void newEmptyFileRequested(); // sending to FileManager that we are creating an empty file
+public slots:
+    void fileHasBeenOpened(QString &);
+    void saveFileTriggered();
+    void newEmptyFile(); // receiving from CustomMenu that we need to create a new empty file
 
-    protected:
-        void wheelEvent(QWheelEvent *event) override;
-        void paintEvent(QPaintEvent *event) override;
+protected:
+    void wheelEvent(QWheelEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 };
